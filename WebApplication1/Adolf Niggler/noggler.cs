10daa1d3e7a8173ffce4d4e2bb0249cde97f9ed0
JR <jash.ramani8866@gmail.com>
2025-12-10 16:13:16 +0530

    noggler file

--- WebApplication1/Adolf Niggler/noggler.cs
+++ WebApplication1/Adolf Niggler/noggler.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApplication1.Adolf_Niggler
+﻿using System;
+
+
+namespace WebApplication1.Adolf_Niggler
 {
+    
     public class noggler
-    {
-    }
+    {  
+       Console.WriteLine("This will be on one line.");
+        
+        
 }
